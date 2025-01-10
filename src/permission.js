c30c031da71925a2b878ff49009040a9a57c0d19
lp <lp@123.cn>
2025-01-10 22:14:17 +0800

    优化数据-数据持久化

--- src/permission.js
+++ src/permission.js
@@ -1,30 +1,26 @@
 import router from "@/router/index";
 import store from "@/store";
+import { getAdminInfo } from "@/utils/storage";
 let whitelist = ["/login", "/regist", "/error", "/", "/index"];
 import { Message } from "element-ui";
 // 导航守卫
 router.beforeEach(async (to, from, next) => {
-  console.log("来自哪里：", from.path);
-  console.log("去往何方：", to.path);
-  let admin = JSON.parse(window.localStorage.getItem("access-admin"));
-  console.log("admin:", admin);
+  let admin = getAdminInfo();
+
   if (admin) {
     //已登录
     if (to.path === "/login") {
       next("/");
     } else {
-      if (!store.state.user.roleId) {
+      if (!store.getters.roleId) {
         //获取用户信息
         console.log("没有用户信息");
-        await store.dispatch("user/GetInfo", admin.id);
-        console.log(store.state.user.roleId);
+        await store.dispatch("user/GetInfo", admin.userId);
         //动态生成菜单路由
-        await store.dispatch("menu/loadMenu", store.state.user.roleId);
-        console.log(" router.getRoutes();", router.getRoutes());
+        await store.dispatch("menu/loadMenu", store.getters.roleId);
         next(to.path);
         // }
       } else {
-        console.log("else");
         next(); // 动态路由已加载，直接继续导航
       }
     }
